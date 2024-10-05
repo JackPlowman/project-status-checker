2b14c35131fe81cc945d3e59f731e2f77937f031
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-05 21:17:10 +0100

    build: Optimize Dockerfile and add run script (#67)
    # Description
    
    This PR updates the Dockerfile to implement a multi-stage build process,
    improving the image size and security. The build stage now exports a
    requirements.txt file, which is then used in the final stage to install
    dependencies.
    
    A new run.sh script is introduced as the entrypoint for the container.
    This script handles different execution environments, particularly
    adjusting the working directory when running in GitHub Actions.
    
    The changes aim to optimize the Docker image and provide flexibility for
    running the checker in various contexts, including CI/CD pipelines.
    
    fixes #66

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e +x
+
+if [ "$CI" = "true" ]; then
+  # if running in GitHub Actions, change to the root of the repository
+  cd ..
+  cd ..
+fi
+
+# Run the analyser
+python -m checker
+
+# if [ "$CI" = "true" ]; then
+#   # if running in GitHub Actions, copy the output to the output directory
+#   cp original_files github/workspace/output_files
+# fi
