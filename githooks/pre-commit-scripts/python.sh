41368cc3dff327b6662cd9e4764719dfbed81341
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-10 20:48:38 +0100

    chore: GitHooks Only Trigger On Folder Changes (#83)
    # Description
    
    Only run certain GitHooks on relevant folder changes
    
    fixes #82

--- githooks/pre-commit-scripts/python.sh
+++ githooks/pre-commit-scripts/python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e +x
 
-just install
-just ruff-fix
+# Check if there are any changes in the watched folder
+if git diff --cached --name-only | grep -q "^checker/"; then
+  just install
+  just ruff-fix
+fi
