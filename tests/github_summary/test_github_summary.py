f264d08f68ec9c81c83b28f845166c56e51b5cdc
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-01 11:37:41 +0000

    test: Add GitHub Summary Tests (#185)
    # Pull Request
    
    ## Description
    
    This pull request includes updates to dependencies and the addition of a
    new test for GitHub summary functionality. The most important changes
    are listed below:
    
    Dependency updates:
    *
    [`pyproject.toml`](diffhunk://#diff-50c86b7ed8ac2cf95bd48334961bf0530cdc77b5a56f852c5c61b89d735fd711R18-R21):
    Added `Markdown` and `beautifulsoup4` to the test dependencies.
    
    New tests:
    *
    [`tests/github_summary/test_github_summary.py`](diffhunk://#diff-09e417d5d30335b732ffaec77ca49fdd02ed7fda12ced3d4d6cd5845cdb99504R1-R19):
    Added a new test `test_github_summary` to verify the correctness of
    GitHub action summary markdown conversion to HTML and the presence of
    expected headers and row count.
    
    Fixes #153

---
+++ tests/github_summary/test_github_summary.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+from markdown import markdown
+
+
+def test_github_summary() -> None:
+    # Act
+    with Path("tests/github_summary/GITHUB_ACTION_SUMMARY.md").open() as file:
+        summary_markdown = file.read()
+    table_html = markdown(summary_markdown, extensions=["markdown.extensions.tables"])
+    bs4_html = BeautifulSoup(table_html, "html.parser")
+    headers = bs4_html.find_all("th")
+    # Assert Headers
+    headers_text = [header.text for header in headers]
+    assert headers_text == ["", "URL Address", "Status Code", "Success"]
+    # Assert row count
+    rows = bs4_html.find_all("tr")
+    assert len(rows) > 3
