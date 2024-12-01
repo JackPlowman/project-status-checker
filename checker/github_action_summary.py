6d2791f3000fbbb36a22368c6a27d9af78eedb93
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-12-01 11:20:30 +0000

    update

--- checker/github_action_summary.py
+++ checker/github_action_summary.py
@@ -19,28 +19,18 @@ def generate_action_summary(results: list[URLCheckResult]) -> None:
         logger.debug("Not running in GitHub Actions, skipping generating action summary")
         return
     logger.debug("Generating action summary")
-
+    # Generate the markdown contents
     markdown_file = MdUtils(file_name="markdown.md", title="Github Action Summary")
-
     table_headers = ["URL Address", "Status Code", "Success"]
     list_of_strings = [*table_headers]
     for result in results:
         list_of_strings.extend([result.url.address, result.status_code, result.success])
-    logger.warning(
-        "List of strings",
-        list_of_strings=list_of_strings,
-        length=len(list_of_strings),
-        type=type(list_of_strings),
-        columns=len(table_headers),
-        rows=len(list_of_strings) / len(table_headers),
-        rows_type=type(len(list_of_strings) / len(table_headers)),
-    )
     markdown_file.new_table(
         columns=len(table_headers),
         rows=int(len(list_of_strings) / len(table_headers)),
         text=list_of_strings,
         text_align="center",
     )
-
+    # Write the markdown contents to the file
     with Path(environ["GITHUB_STEP_SUMMARY"]).open("w") as file:
         file.write(markdown_file.get_md_text())
---
+++ checker/tests/test_github_action_summary.py
@@ -0,0 +1,46 @@
+from os import environ
+from unittest.mock import MagicMock, patch
+
+from checker.github_action_summary import generate_action_summary
+from checker.url import URL
+from checker.url_check_result import URLCheckResult
+
+FILE_PATH = "checker.github_action_summary"
+
+
+@patch(f"{FILE_PATH}.getenv", return_value="true")
+@patch(f"{FILE_PATH}.Path.open", new_callable=MagicMock)
+def test_generate_action_summary(mock_open: MagicMock, mock_getenv: MagicMock) -> None:
+    """Test the generate_action_summary function."""
+    # Arrange
+    environ["GITHUB_STEP_SUMMARY"] = "dummy_path"
+    url1 = URL("http://example.com", 200)
+    url2 = URL("http://example.org", 404)
+    result1 = URLCheckResult(url1, 200, True)  # noqa: FBT003
+    result2 = URLCheckResult(url2, 404, True)  # noqa: FBT003
+    results = [result1, result2]
+    mock_file = MagicMock()
+    mock_open.return_value.__enter__.return_value = mock_file
+    # Act
+    generate_action_summary(results)
+    # Assert
+    mock_open.assert_called_once_with("w")
+    written_content = mock_file.write.call_args[0][0]
+    assert written_content == (
+        "\nGithub Action Summary\n=====================\n\n"
+        "|URL Address|Status Code|Success|\n| :---: | :---: | :---: |\n"
+        "|http://example.com|200|True|\n|http://example.org|404|True|\n"
+    )
+    # Clean up
+    del environ["GITHUB_STEP_SUMMARY"]
+
+
+@patch(f"{FILE_PATH}.getenv", return_value="false")
+def test_generate_action_summary_not_in_github_actions(mock_getenv: MagicMock) -> None:
+    """Test the generate_action_summary function when not in GitHub Actions."""
+    # Arrange
+    results = []
+    # Act
+    generate_action_summary(results)
+    # Assert
+    mock_getenv.assert_called_once_with("GITHUB_ACTION", "false")
